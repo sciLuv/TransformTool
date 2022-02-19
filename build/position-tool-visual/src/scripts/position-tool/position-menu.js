ff9d0b4391a66b8bb491ec2880b104e53536780c
unknown <sci.luv.inc@gmail.com>
2022-02-19 20:40:06 +0100

    finish first version of visual fonctionnig of flex position menu

--- build/position-tool-visual/src/scripts/position-tool/position-menu.js
+++ build/position-tool-visual/src/scripts/position-tool/position-menu.js
@@ -55,7 +55,7 @@ function baseMenu(){
 
         posMenu.removeAttribute("base");
         posMenu.setAttribute(posContainerSetting.selectPos, "");
-        fetch('data/position/' + posContainerSetting.selectPos + '.txt')
+        fetch('data/position/' + posContainerSetting.selectPos + '.html')
         .then(response => response.text())
         .then(data => {
             posMenu.innerHTML = data;
@@ -86,7 +86,7 @@ function whenFreeIsSelect(){
         posMenu.setAttribute("base", "");
         posMenu.innerHTML = "";
         posMenu.removeAttribute("free");
-        fetch('data/position/base.txt')
+        fetch('data/position/base.html')
         .then(response => response.text())
         .then(data => {
             posMenu.innerHTML = data;
@@ -103,7 +103,7 @@ function whenFlexIsSelect(){
         posMenu.setAttribute("base", "");
         posMenu.innerHTML = "";
         posMenu.removeAttribute("flex");
-        fetch('data/position/base.txt')
+        fetch('data/position/base.html')
         .then(response => response.text())
         .then(data => {
             posMenu.innerHTML = data;
@@ -115,19 +115,61 @@ function whenFlexIsSelect(){
 }
 
 function flexBtns(){
-    let selectFlexXY = document.getElementById("interuptor-flex-xy");
-    let interuptorFlexXY = document.getElementById("flex-axes");
+    let selectFlexXY = document.getElementById("interuptor-flex-direction");
+    let interuptorFlexRowColumn = document.getElementById("flex-positions");
     
     let flexXY = false;
     
     selectFlexXY.addEventListener('click', function(){
         if(flexXY == false){
-            interuptorFlexXY.setAttribute("active","");
+            interuptorFlexRowColumn.setAttribute("active","");
             flexXY = true;
         }
         else{
-            interuptorFlexXY.removeAttribute("active");
+            interuptorFlexRowColumn.removeAttribute("active");
             flexXY = false;
+        }
+    })
+
+    let selectFlexAxe1 = document.getElementById("first-axe-interuptor");
+    let interuptorFlexStartEnd1 = document.getElementById("first-axe-under-interuptor");
+    
+    let flexStartEnd1 = "start";
+    
+    selectFlexAxe1.addEventListener('click', function(){
+        if(flexStartEnd1 == "start"){
+            interuptorFlexStartEnd1.setAttribute("middle","");
+            flexStartEnd1 = "middle";
+        }
+        else if(flexStartEnd1 == "middle"){
+            interuptorFlexStartEnd1.removeAttribute("middle");
+            interuptorFlexStartEnd1.setAttribute("end","");
+            flexStartEnd1 = "end";
+        }
+        else{
+            interuptorFlexStartEnd1.removeAttribute("end");
+            flexStartEnd1 = "start";
+        }
+    })
+
+    let selectFlexAxe2 = document.getElementById("second-axe-interuptor");
+    let interuptorFlexStartEnd2 = document.getElementById("second-axe-under-interuptor");
+    
+    let flexStartEnd2 = "start";
+    
+    selectFlexAxe2.addEventListener('click', function(){
+        if(flexStartEnd2 == "start"){
+            interuptorFlexStartEnd2.setAttribute("middle","");
+            flexStartEnd2 = "middle";
+        }
+        else if(flexStartEnd2 == "middle"){
+            interuptorFlexStartEnd2.removeAttribute("middle");
+            interuptorFlexStartEnd2.setAttribute("end","");
+            flexStartEnd2 = "end";
+        }
+        else{
+            interuptorFlexStartEnd2.removeAttribute("end");
+            flexStartEnd2 = "start";
         }
     })
 }
