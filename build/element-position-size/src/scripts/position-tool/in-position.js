17c4cda6e48340c83caf58d9a6e55e2d30a94e1e
unknown <sci.luv.inc@gmail.com>
2022-04-17 12:09:27 +0200

    finish first version of functionnal size select tool, for all type of display except grid

--- build/element-position-size/src/scripts/position-tool/in-position.js
+++ build/element-position-size/src/scripts/position-tool/in-position.js
@@ -6,8 +6,6 @@ let sizeIFList = [];
 
 function createSize(){
     for(l=0; l<=elemList.length-1; l++){ 
-
-        console.log("test size");
         let elemNum = l;
 
         if(sizeIFList[elemNum] == undefined){
@@ -15,107 +13,130 @@ function createSize(){
                 existing : false,
                 height : {
                     num : elemNum,
-                    interuptor : false
+                    interuptor1 : false,
+                    interuptor2 : false
                 },
                 width : {
                     num : elemNum,
-                    interuptor : false,
+                    interuptor1 : false,
+                    interuptor2 : false
                 }
             }
         }
         else{
             sizeIFList[elemNum]["height"] = {
                 num : elemNum,
-                interuptor : false
+                interuptor1 : false,
+                interuptor2 : false
             }
             sizeIFList[elemNum]["width"] = {
                 num : elemNum,
-                interuptor : false
+                interuptor1 : false,
+                interuptor2 : false
             }
         }
+        if(sizeIFList[elemNum].existing == false){
+            let initialHeight1Placement;
+            let initialHeight2Placement;
+            
+            let initialWidth1Placement;
+            let initialWidth2Placement;
+        }
 
-        let initialHeight1Placement;
-        let initialHeight2Placement;
-        
-        let initialWidth1Placement;
-        let initialWidth2Placement;
-
-        heightBtns[(sizeIFList[elemNum].height.num*2)].addEventListener("click",function(){
-            elemList[sizeIFList[elemNum].height.num].size.height++;
-            document.getElementById(elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
-            document.getElementById("if-" + elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
-
+        heightBtns[(sizeIFList[elemNum].height.num*2)].addEventListener("mousedown",function(event){
+            initialHeight1Placement = event.clientY; 
+            sizeIFList[elemNum].height.interuptor1 = true;
         });
-        widthBtns[(sizeIFList[elemNum].width.num*2)].addEventListener("click",function(){
-            elemList[sizeIFList[elemNum].width.num].size.width++;
-            document.getElementById(elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
-            document.getElementById("if-" + elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
-
-        });
-
         heightBtns[(sizeIFList[elemNum].height.num*2)+1].addEventListener("mousedown",function(event){
             initialHeight2Placement = event.clientY; 
-            sizeIFList[elemNum].height.interuptor = true;
-            console.log(sizeIFList[elemNum].height.interuptor);
+            sizeIFList[elemNum].height.interuptor2 = true;
+        });
+
+        widthBtns[(sizeIFList[elemNum].width.num*2)].addEventListener("mousedown",function(event){
+            initialWidth1Placement = event.clientX; 
+            sizeIFList[elemNum].width.interuptor1 = true; 
         });
         widthBtns[(sizeIFList[elemNum].width.num*2)+1].addEventListener("mousedown",function(event){
-            console.log("mouse down " + elemNum);
             initialWidth2Placement = event.clientX; 
-            console.log(initialWidth2Placement);
-            sizeIFList[elemNum].width.interuptor = true;
-            console.log(sizeIFList[elemNum].width.interuptor);
+            sizeIFList[elemNum].width.interuptor2 = true;
         });
+
         if(sizeIFList[elemNum].existing == false){
             body.addEventListener("mousemove", sizeMove);
             function sizeMove(event){
                 if(elemList[elemNum] != undefined){
-                    console.log("test delete size " + elemNum);
-                    if(sizeIFList[elemNum].width.interuptor == true){
-                        console.log("body miuseMove");
-                        let placement = event.clientX;
-                        console.log(placement);
-                        if(placement > initialWidth2Placement){
-                            elemList[elemNum].size.width += (placement - initialWidth2Placement);
-                        
+                    if(posSetting.display.display != "grid"){
+                        if(sizeIFList[elemNum].width.interuptor2 == true){
+                            let placement = event.clientX;
+                            if(placement > initialWidth2Placement){
+                                elemList[elemNum].size.width += (placement - initialWidth2Placement); 
+                            }
+                            if(placement < initialWidth2Placement){
+                                elemList[elemNum].size.width -= (initialWidth2Placement - placement);
+                            }
+                            initialWidth2Placement = placement;
+                            document.getElementById(elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
+                            document.getElementById("if-" + elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
                         }
-                        if(placement < initialWidth2Placement){
-                            elemList[elemNum].size.width -= (initialWidth2Placement - placement);
+                        if(sizeIFList[elemNum].width.interuptor1 == true){
+                            let placement = event.clientX;
+                            if(placement < initialWidth1Placement){
+                                elemList[elemNum].size.width += (placement - initialWidth1Placement);
+                            }
+                            if(placement > initialWidth1Placement){
+                                elemList[elemNum].size.width -= (initialWidth1Placement - placement);
+                            }
+                            initialWidth1Placement = placement;
+                            document.getElementById(elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
+                            document.getElementById("if-" + elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
                         }
-                        initialWidth2Placement = placement;
-                        document.getElementById(elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
-                        document.getElementById("if-" + elemList[sizeIFList[elemNum].width.num].id.name).style.width = elemList[sizeIFList[elemNum].width.num].size.width + "px";
-                    }
-        
-                    if(sizeIFList[elemNum].height.interuptor == true){
-                        let placement = event.clientY;
-                        if(placement > initialHeight2Placement){
-                            elemList[elemNum].size.height += (placement - initialHeight2Placement);
-                        
+            
+                        if(sizeIFList[elemNum].height.interuptor2 == true){
+                            let placement = event.clientY;
+                            if(placement > initialHeight2Placement){
+                                elemList[elemNum].size.height += (placement - initialHeight2Placement);
+                            
+                            }
+                            if(placement < initialHeight2Placement){
+                                elemList[elemNum].size.height -= (initialHeight2Placement - placement);
+                            }
+                            initialHeight2Placement = placement;
+                            document.getElementById(elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
+                            document.getElementById("if-" + elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
                         }
-                        if(placement < initialHeight2Placement){
-                            elemList[elemNum].size.height -= (initialHeight2Placement - placement);
+                        if(sizeIFList[elemNum].height.interuptor1 == true){
+                            let placement = event.clientY;
+                            if(placement > initialHeight1Placement){
+                                elemList[elemNum].size.height += (placement - initialHeight1Placement);
+                            
+                            }
+                            if(placement < initialHeight1Placement){
+                                elemList[elemNum].size.height -= (initialHeight1Placement - placement);
+                            }
+                            initialHeight1Placement = placement;
+                            document.getElementById(elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
+                            document.getElementById("if-" + elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
                         }
-                        initialHeight2Placement = placement;
-                        document.getElementById(elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
-                        document.getElementById("if-" + elemList[sizeIFList[elemNum].height.num].id.name).style.height = elemList[sizeIFList[elemNum].height.num].size.height + "px";
                     }
                 }
             }
             body.addEventListener("mouseup", sizeEndMove);
             function sizeEndMove(){
                 if(elemList[elemNum] != undefined){
-                    console.log("body mouseUp " + elemNum);
-                        if(sizeIFList[elemNum].width.interuptor == true){
-                            console.log('test');
-                            sizeIFList[elemNum].width.interuptor = false;
+                        if(sizeIFList[elemNum].width.interuptor1 == true){
+                            sizeIFList[elemNum].width.interuptor1 = false;
                         }
-                        if(sizeIFList[elemNum].height.interuptor == true){
-                            sizeIFList[elemNum].height.interuptor = false;
+                        if(sizeIFList[elemNum].width.interuptor2 == true){
+                            sizeIFList[elemNum].width.interuptor2 = false;
+                        }
+                        if(sizeIFList[elemNum].height.interuptor1 == true){
+                            sizeIFList[elemNum].height.interuptor1 = false;
+                        }
+                        if(sizeIFList[elemNum].height.interuptor2 == true){
+                            sizeIFList[elemNum].height.interuptor2 = false;
                         }
                 }
             }
-            //body.removeEventListener("mousemove", sizeMove);
-            //body.removeEventListener("mouseup", sizeEndMove);
             sizeIFList[elemNum].existing = true;
         }
     }
